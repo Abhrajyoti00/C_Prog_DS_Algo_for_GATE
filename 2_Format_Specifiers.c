3a4cede0c11747d9702e96b45fe77b79e61c3b7b
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-08-04 13:06:06 +0530

    Added format Specifiers

---
+++ 2_Format_Specifiers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    printf("%d\n", 10);
+    printf("%6d\n", 10);
+    printf("%d\n", 10);
+    printf("%d\n", 10);
+    printf("%d\n", 10);
+    printf("%d\n", 10);
+    printf("%d\n", 10);
+    printf("%d\n", 10);
+}
