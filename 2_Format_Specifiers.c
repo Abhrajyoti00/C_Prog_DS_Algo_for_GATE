253af68a2a327b4baa2cbcd777a03254fa771bde
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-08-04 13:28:12 +0530

    Modified Format Specifiers

--- 2_Format_Specifiers.c
+++ 2_Format_Specifiers.c
@@ -2,10 +2,12 @@
 void main(){
     printf("%d\n", 10);
     printf("%6d\n", 10);
-    printf("%d\n", 10);
-    printf("%d\n", 10);
-    printf("%d\n", 10);
-    printf("%d\n", 10);
+    printf("%f\n", 10.9);
+    printf("%12f\n", 10.6);
+    printf("%.2f\n", 10.5);
+    printf("%.2f\n", 10.56654);
+    printf("%.2f\n", 10);
+    printf("%6.2f\n", 10.7);
     printf("%d\n", 10);
     printf("%d\n", 10);
 }
