ae795324ac7890d3a4b179ad59c20e2917172e81
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-08-07 09:26:13 +0530

    String qs added

---
+++ 15_StringQs.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#define scanf "%s Ravindra"
+void main(){
+    int n;
+    printf(scanf,scanf); // o/p : %s Ravindra Ravindra
+    // for(n=3;n!=0;n--)
+    //     printf("n = %d", n--);  //Infinite loop
+}
