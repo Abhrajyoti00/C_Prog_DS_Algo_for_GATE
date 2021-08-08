0662a53fdeca74e8d2fe991ca282596ccef1bc5b
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-08-08 08:40:20 +0530

    Added success status to fseek prg

--- 17_fseek_ftell.c
+++ 17_fseek_ftell.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 void main(){
     FILE *fp;
-    int len;
+    int len, success;
     fp = fopen("TestIp.txt", "r");
     if(fp == NULL)
         printf("Error opening file\n");
     else{
-        fseek(fp, 0, SEEK_END);
+        success = fseek(fp, 0, SEEK_END);
         len = ftell(fp);
     }
     fclose(fp);
+    printf("Success status %d\n", success);
     printf("Total size of TestIp.txt = %d bytes \n", len);
 }
