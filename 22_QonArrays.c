2fc43543e22e60385ecc8cc8d5aba39b33ee7bb6
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-08-11 12:44:03 +0530

    Q on arrays modified

--- 22_QonArrays.c
+++ 22_QonArrays.c
@@ -1,25 +1,45 @@
 #include<stdio.h>
-void fun(int arr[])  // Here pointer is passed
-{
-int i;
-int arr_size = sizeof(arr)/sizeof(arr[0]);
+// void fun(int arr[])  // Here pointer is passed
+// {
+// int i;
+// int arr_size = sizeof(arr)/sizeof(arr[0]);
 
-printf("Size of int = %d\n", sizeof(int));
-printf("Size of arr = %d\n", sizeof(arr)); // It actually gives pointer size, not array size 
-printf("Size of arr[0] = %d\n", sizeof(arr[0]));
-for (i = 0; i < arr_size; i++)
- printf("%d ", arr[i]);
-}
+// printf("Size of int = %d\n", sizeof(int));
+// printf("Size of arr = %d\n", sizeof(arr)); // It actually gives pointer size, not array size 
+// printf("Size of arr[0] = %d\n", sizeof(arr[0]));
+// for (i = 0; i < arr_size; i++)
+//  printf("%d ", arr[i]);
+// }
+// int main()
+// {
+//  int i,*p;
+//  int arr[4] = {10, 20 ,30, 40};
+//  printf("size of pointer = %d", sizeof(p));
+
+//  printf("intitally :- \n");
+//  printf("Size of arr = %d\n", sizeof(arr));
+//  printf("Size of arr[0] = %d\n", sizeof(arr[0]));
+//  printf("After Passing to fun :- \n");
+
+//  fun(arr);
+//  fun(arr);}
+
+// int main()
+// {
+//  char *str; 
+//  str = "GfG";     /* Stored in read only part of data segment */
+//  *(str+1) = 'n'; /* Problem:  trying to modify read only memory */
+//  printf("%s", str);
+//  getchar();
+//  return 0;
+// }   /// ERROR
+
 int main()
 {
- int i,*p;
- int arr[4] = {10, 20 ,30, 40};
- printf("size of pointer = %d", sizeof(p));
+ char str[] = "GfG";  /* Stored in stack segment like other auto variables */
+ *(str+1) = 'n';   /* No problem: String is now GnG */
+ printf("%s", str);
 
- printf("intitally :- \n");
- printf("Size of arr = %d\n", sizeof(arr));
- printf("Size of arr[0] = %d\n", sizeof(arr[0]));
- printf("After Passing to fun :- \n");
-
- fun(arr);
- fun(arr);}
+ getchar();
+ return 0;
+}
---
+++ 24_BinarySearchTree.c
@@ -0,0 +1,127 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *left, *right;
+};
+
+struct node* AddNode(int c){
+    struct node *newNode = (struct node*) malloc(sizeof(struct node));
+    newNode->data = c;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+void PreOrder(struct node *t){
+    if(t){
+        printf("%d, ", t->data);
+        PreOrder(t->left);
+        PreOrder(t->right);
+    }
+    else
+        return;
+}
+
+void InOrder(struct node *t){
+    if(t){
+        InOrder(t->left);
+        printf("%d, ", t->data);
+        InOrder(t->right);
+    }
+    else
+        return;
+}
+
+void PostOrder(struct node *t){
+    if(t){
+        PostOrder(t->left);
+        PostOrder(t->right);
+        printf("%d, ", t->data);
+    }
+    else
+        return;   
+}
+
+int NN(struct node *root){ // No of Nodes
+    if(root == NULL)
+        return 0;
+    else{
+        return (1+NN(root->left)+NN(root->right));
+    }
+}
+int NL(struct node *root){   //No of Leaf
+    if(root == NULL)
+        return 0;
+    if(root->left == NULL && root->right == NULL)
+        return 1;
+    else
+        return NL(root->left) + NL(root->right);
+}
+int NNL(struct node *root){   //No of Non - Leaf
+    if(root == NULL)
+        return 0;
+    if(root->left == NULL && root->right == NULL)
+        return 0;
+    else
+        return 1+NNL(root->left) + NNL(root->right);
+}
+int FN(struct node *root){   //No of Full Nodes
+    if(root == NULL) 
+        return 0;
+    if(root->left == NULL && root->right == NULL)   // Leaf
+        return 0;
+    return (FN(root->left) + FN(root->right) + ((root->right && root->left) ? 1:0));
+}
+int H(struct node *root){
+    if(!root)
+        return 0;
+    if(!root->left && !root->right)
+        return 0;
+    int l = H(root->left);
+    int r = H(root->right);
+    return 1+ ((l>r)?l:r);
+}
+
+
+void main(){
+    struct node* root = AddNode(1);
+    /* following is the tree after above statement
+         1
+        / \
+      NULL NULL
+    */
+ 
+    root->left = AddNode(2);
+    root->right = AddNode(3);
+    /* 2 and 3 become left and right children of 1
+            1
+         /    \
+        2      3
+      /  \    /  \
+   NULL NULL NULL NULL
+    */
+ 
+    root->left->left = AddNode(4);
+    /* 4 becomes left child of 2
+             1
+         /    \
+        2      3
+      /  \    /  \
+     4 NULL NULL NULL
+    / \
+ NULL NULL
+    */
+   printf("\nThe PreOrder Traversal of the Binary Search Tree is : \n");
+   PreOrder(root);
+   printf("\nThe InOrder Traversal of the Binary Search Tree is : \n");
+   InOrder(root);
+   printf("\nThe PostOrder Traversal of the Binary Search Tree is : \n");
+   PostOrder(root);
+
+    printf("\nThe number of nodes in the tree :- %d\n", NN(root));
+    printf("\nThe number of leaf nodes in the tree :- %d\n", NL(root));
+    printf("\nThe number of non - leaf nodes in the tree :- %d\n", NNL(root));
+    printf("\n The no of full nodes are %d\n", FN(root));
+    printf("\n The height of the Binary Search Tree is %d\n", H(root));
+
+}
