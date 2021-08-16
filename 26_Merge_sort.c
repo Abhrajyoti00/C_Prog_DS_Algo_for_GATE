2e7e7cd447c1b450a220bcadecd83a6e9bdc0877
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-08-16 11:30:46 +0530

    Main function of Merge Sort added

--- 26_Merge_sort.c
+++ 26_Merge_sort.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<math.h>
+#include <math.h>
 void Merge(int arr[], int p, int q, int r)
 {
     int n1 = q - p + 1;
@@ -13,22 +13,41 @@ void Merge(int arr[], int p, int q, int r)
     L[n1 + 1] = __INT_MAX__;
     R[n2 + 1] = __INT_MAX__;
     i = j = 1;
-    for (int k = p; k < r; k++){
-        if(L[i] <= R[j]){
+    for (int k = p; k < r; k++)
+    {
+        if (L[i] <= R[j])
+        {
             arr[k] = L[i];
-            i+=1;
+            i += 1;
         }
-        else{
+        else
+        {
             arr[k] = R[j];
-            j+=1;
+            j += 1;
         }
     }
 }
-void MergeSort(int arr[], int p, int r){
-    if(p<r){
-        int q = (int)floor((p+q)/2);
-        MergeSort(arr, p,q );
-        MergeSort(arr, q+1,r );
+void MergeSort(int arr[], int p, int r)
+{
+    if (p < r)
+    {
+        int q = (int)floor((p + q) / 2);
+        MergeSort(arr, p, q);
+        MergeSort(arr, q + 1, r);
         Merge(arr, p, q, r);
     }
+}
+
+int main()
+{
+    int i;
+    int n;
+    scanf("%d", &n);
+    int arr[n + 1];
+    for (i = 1; i <= n; i++)
+        scanf("%d", &arr[i]);
+    MergeSort(arr, 1, n);
+    printf("Array after sorting \n");
+    for (i = 1; i <= n; i++)
+        printf("%d ", arr[i]);
 }
